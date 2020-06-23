f1d4132bf154fc64e9620b9bbd9d935ead6d0e5a
Salathiel Genese <salathielgenese@gmail.com>
2020-06-23 07:47:34 +0400

    [SPICEX-1] feat: placeholder home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
+  providers: [],
+  bootstrap: [
+    AppComponent,
+  ],
   declarations: [
-    AppComponent
+    AppComponent,
+    NotFoundComponent,
   ],
   imports: [
-    BrowserModule
+    RouterModule,
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: HomeComponent,
+      },
+      {
+        path: '**',
+        component: NotFoundComponent,
+      },
+    ]),
   ],
-  providers: [],
-  bootstrap: [AppComponent]
 })
 export class AppModule { }
